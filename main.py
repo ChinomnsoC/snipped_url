665891c35712617984dc66b22ff6b124b4483af4
ChinomnsoC <chinomnsoc@gmail.com>
2022-08-26 17:09:59 +0100

    set up validators, added schema file to structure database, config file to include cache

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
+import validators
+from fastapi import FastAPI, HTTPException
+from . import schemas
 
-def id_to_short_url(id):
-    map_url = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    short_url = ""
-
-    # for each digit find the base 62
-    while (id > 0):
-        short_url += map_url[id % 62]
-        id //= 62
-
-    # reversing the short_url
-    return short_url[len(short_url):: -1]
+app = FastAPI()
 
 
-def short_url_to_id(short_url):
-    url_id = 0
-    for i in short_url:
-        val_i = ord(i)
-        if ord('a') <= val_i <= ord('z'):
-            url_id = url_id * 62 + val_i - ord('a')
-        elif ord('A') <= val_i <= ord('Z'):
-            url_id = url_id * 62 + val_i - ord('A') + 26
-        else:
-            url_id = url_id * 62 + val_i - ord('0') + 52
-    return url_id
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
 
 
-# Press the green button in the gutter to run the script.
+@app.get("/")
+def read_root():
+    return "Welcome to the snipped_url App"
 
-if __name__ == "__main__":
-    url_id = 12345
-    short_urls = id_to_short_url(url_id)
-    print("Short URL from 12345 is : ", short_urls)
-    print("ID from", short_urls, "is : ", short_url_to_id(short_urls))
 
+@app.post("/url")
+def shorten_url(url: schemas.URLBase):
+    if validators.url(url.target_url):
+        pass
+    else:
+        raise_bad_request(message="Ooopsies :(, Your provided URL is not valid")
+    return f"TODO: Create database entry for: {url.target_url}"
+#
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
---
+++ schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class URLBase(BaseModel):
+    target_url: str
+
+
+class URL(URLBase):
+    is_active: bool
+    clicks: int
+    creator_ip_address: str
+    clicker_ip_address: str
+
+    class Config:
+        orm_mode = True
+
+
+class URLInfo(URL):
+    url: str
+    admin_url: str
